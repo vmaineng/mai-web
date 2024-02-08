05e498cbc373ce97d1d1c38a571e04f233a6afa7
Mai Vang <vmaineng@gmail.com>
2024-02-08 15:31:09 -0800

    added in font size

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -6,19 +6,20 @@ import Image from "next/image";
 const Banner = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             {" "}
             Hello, I'm Mai
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Happy that you're here!
           </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"> Reach Me</button>
         </div>
-        <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black"> Reach Me</button>
         </div>
+       
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import Banner from "./components/Banner";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-black">
+    <div class="container mx-auto px-12 py-4">
     <Banner />
+    </div>
     </main>
   );
 }
