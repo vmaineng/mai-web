866b13b4052597b93c56db56a5458b3c3f8aee13
Mai Vang <vmaineng@gmail.com>
2024-02-07 18:30:16 -0800

    first commit

---
+++ app/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//functional component
+
+const Banner = () => {
+  return (
+    <section>
+          <h1 className="text-white mb-4 text-6xl font-extrabold"> Hello, I'm Mai</h1>
+          <p className ="text-[#ADB7BE]">
+            Happy that you're here!
+          </p>
+    </section>
+  )
+}
+
+export default Banner
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Banner from "./components/Banner";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-black container mx-auto px-12 py-4">
+    <Banner />
+    </main>
+  );
+}
