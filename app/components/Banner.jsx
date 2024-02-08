a1a4683d99005af5404d4a475c965b73e55fed67
Mai Vang <vmaineng@gmail.com>
2024-02-08 15:38:09 -0800

    added in banner complete

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
 
 //functional component
 
@@ -9,8 +11,26 @@ const Banner = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            {" "}
-            Hello, I'm Mai
+           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+           Hello, I'm{" "}
+           </span>
+           <br/>
+           <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Mai',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Software Engineer',
+        1000,
+        'Software Developer',
+        1000,
+        'Web Developer',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Happy that you're here!
