d9d95e56d9ad5ec40947663f25ea0714ee780db0
Mai Vang <vmaineng@gmail.com>
2024-02-08 15:21:55 -0800

    added in text

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -6,24 +6,30 @@ import Image from "next/image";
 const Banner = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className = "col-span-7 place-self-center">
-      <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-        {" "}
-        Hello, I'm Mai
-      </h1>
-      <p className="text-[#ADB7BE] text-lg lg:text-xl">
-        Happy that you're here!
-      </p>
-      </div>
-      <div className="col-span-5">
-        <Image 
-        src="/images/photo.png"
-        alt="photo"
-        width={300}
-        height={300}
-        />
-      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            {" "}
+            Hello, I'm Mai
+          </h1>
+          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+            Happy that you're here!
+          </p>
+        </div>
+        <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black"> Reach Me</button>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/photo.png"
+              alt="photo"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
