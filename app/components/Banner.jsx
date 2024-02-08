3691867dbaef72bc2518a72e8edc5c1a297b9509
Mai Vang <vmaineng@gmail.com>
2024-02-07 18:37:54 -0800

    added in photo of me

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 //functional component
 
 const Banner = () => {
   return (
     <section>
-          <h1 className="text-white mb-4 text-6xl font-extrabold"> Hello, I'm Mai</h1>
-          <p className ="text-[#ADB7BE]">
-            Happy that you're here!
-          </p>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className = "col-span-7 place-self-center">
+      <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+        {" "}
+        Hello, I'm Mai
+      </h1>
+      <p className="text-[#ADB7BE] text-lg lg:text-xl">
+        Happy that you're here!
+      </p>
+      </div>
+      <div className="col-span-5">
+        <Image 
+        src="/images/photo.png"
+        alt="photo"
+        width={300}
+        height={300}
+        />
+      </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
