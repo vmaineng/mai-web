343ad5cfcc6fed0105414677eedf794529257bba
Mai Vang <vmaineng@gmail.com>
2024-02-08 16:50:07 -0800

    navbar done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
