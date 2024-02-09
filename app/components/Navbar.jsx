9d6f1604a2eaa64b9e7c52db35de1df943423cfc
Mai Vang <vmaineng@gmail.com>
2024-02-08 16:14:04 -0800

    added in toggle bar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+"use client"; //turns it into a client component
+import React, { useState } from "react";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
+  const [navbarOpen, setnavbarOpen] = useState(false);
+
   const navLinks = [
     {
       title: "About",
@@ -27,6 +31,17 @@ const Navbar = () => {
           {" "}
           LOGO{" "}
         </Link>
+        <div className="mobile-menu block md:hidden">
+          {navbarOpen ? (
+            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"> 
+    <Bars3Icon className ="h-5 w-5" />
+            </button>
+          ) : (
+            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"> 
+  <XMarkIcon className ="h-5 w-5" />
+            </button>
+          )}
+        </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt:0">
             {navLinks.map((link, index) => (
